a49fb6dd0a07ebc2704ba6442193154eb23b4364
Disha-Jadhav <74053161+Disha-Jadhav@users.noreply.github.com>
2020-12-16 16:26:12 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,34 @@
+class rope
+{
+    constructor(bodyA, pointB)
+    {
+        var options = 
+        {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.Rope = Constraint.create(options);
+        World.add(world, this.Rope);
+    }
+
+    fly()
+    {
+        this.Rope.bodyA = null;
+    }
+ 
+    display()
+    {
+         if(this.Rope.bodyA)
+         {
+            var pointA = this.Rope.bodyA.position;
+            var pointB = this.pointB;
+            fill("black");
+            stroke("white")
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);   
+         } 
+    }
+}
